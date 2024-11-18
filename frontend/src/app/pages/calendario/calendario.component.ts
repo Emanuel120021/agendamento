1e61dd86c8876d81ec5be6ef1704337fe0800a44
emanuel120021 <emanuel.lazaro.52@gmail.com>
2024-11-18 16:51:22 -0300

    Adição do full calendar

--- frontend/src/app/pages/calendario/calendario.component.ts
+++ frontend/src/app/pages/calendario/calendario.component.ts
@@ -1,19 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions } from '@fullcalendar/core/index.js';
+import {
+  CalendarOptions,
+  DateSelectArg,
+  EventClickArg,
+  EventApi,
+} from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
+import ptBrLocale from '@fullcalendar/core/locales/pt-br';
 
 @Component({
   selector: 'app-calendario',
   standalone: true,
-  imports: [FullCalendarModule],
+  imports: [CommonModule, RouterOutlet, FullCalendarModule],
   templateUrl: './calendario.component.html',
-  styleUrl: './calendario.component.scss',
+  styleUrls: ['./calendario.component.scss'],
 })
 export class CalendarioComponent {
+  calendarVisible: boolean = true; // Substituído por uma propriedade boolean
+  currentEvents: EventApi[] = []; // Substituído por uma array simples
   calendarOptions: CalendarOptions = {
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth',
+    },
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin],
-    height: '90vh',
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    height: '80vh',
+    locale: ptBrLocale,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this),
   };
+
+  constructor(private changeDetector: ChangeDetectorRef) {}
+
+  handleCalendarToggle() {
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  handleWeekendsToggle() {
+    this.calendarOptions.weekends = !this.calendarOptions.weekends; // Atualiza diretamente a propriedade
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents = events; // Atualiza diretamente a array
+    this.changeDetector.detectChanges(); // Atualiza a view manualmente se necessário
+  }
 }
